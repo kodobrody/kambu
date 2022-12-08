1f3f4e190863f825cc7cff09d30552336edb4ffa
kodobrody <adam.dominiak94@gmail.com>
2022-12-08 09:59:09 +0100

    updated scss path

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import './components/videoPlayer/videoPlayer.scss'
+import '@/components/VideoPlayer/VideoPlayer.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
